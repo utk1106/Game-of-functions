d301f8a19f95191d221ba3cd342f1df839d215f8
Utkarsh <thaokarutkarsh@gmail.com>
2022-02-17 22:25:23 +0530

    Add files via upload

---
+++ Game of Functions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def listsum(list):
+    ret=0
+    for i in list:
+        ret += i
+    return ret
+
+print (listsum([8,2,3,0,7]))
+
+
+# In[ ]:
+
+
+
+
